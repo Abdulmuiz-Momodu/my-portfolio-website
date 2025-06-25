7fd02152a4346d6625e1d5bd123f1d05658c7dd7
Abdulmuiz-Momodu <ilamosiabdul@gmail.com>
2025-06-25 20:49:11 +0100

    ....

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="footer tracking-widest text-[#A9AABC] text-center hover:text-[#F2B880]">
+    <div className="footer tracking-widest text-[14px] text-[#A9AABC] text-center hover:text-[#F2B880]">
       <a href="https://github.com/Abdulmuiz-Momodu/my-portfolio-website" target="_blank">Created & Designed By Abdulmuiz Momodu</a>
     </div>
   );
